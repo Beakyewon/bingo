fa4fe3f3aff27ae2bc4742aa590ab4a903e6f642
Beakyewon <43024179+Beakyewon@users.noreply.github.com>
2018-11-21 15:56:14 +0900

    5

--- 빙고.c
+++ 빙고.c
@@ -7,7 +7,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int initiate_bingo(int bingo[]); // 빙고 테이블을 초기에 만들어줌 
+int initiate_bingo(int bingo[N]); // 빙고 테이블을 초기에 만들어줌 
 int print_bingo(int bingo[N][N]);    // 빙고 테이블에 현재상황 출력
 int get_number_byMe();     //내가 빙고번호 입력 선택 
 int get_number_byCom();    //컴퓨터가 임의로 빙고 번호 선택 
@@ -21,7 +21,7 @@ int count=0;
 
 int main()
 {
-	int number;
+	int num;
 	int user_count, com_count;
 	
 	initiate_bingo(user_bingo);
@@ -31,26 +31,29 @@ int main()
 	
 		printf("\t[USER]\n");
 			print_bingo(user_bingo);
-			number = get_number_byMe();
-			process_bingo(user_bingo, number);
-			process_bingo(com_bingo, number);
+			num = get_number_byMe();
+			process_bingo(user_bingo, num);
+			process_bingo(com_bingo, num);
 			
-		prinrf("\t[COMPUTER]\n");
+		printf("\t[COMPUTER]\n");
 			print_bingo(com_bingo);
-			number = get_number_byCom();
-			process_bingo(com_bingo, number);
-			process_bingo(user_bingo, number);
+			num = get_number_byCom();
+			process_bingo(com_bingo, num);
+			process_bingo(user_bingo, num);
 			
-	if((user_count==!0)||(com_count==!0))
+	if((user_count==M)||(com_count==M))
 		break;
 	}
+	system("pause");
+	system("cls");
+	return 0;
 }
 
-int initiate_bingo(int bingo[]){
+int initiate_bingo(int bingo[N]){
 	
 	srand((unsigned int)time(NULL));
 	int i,a,b,c;
-	for(i=0;i<N*N;i++){
+	for(i=0; i<N*N; i++){
 		bingo[i] = i+1;
 		
 		a=rand()%(N*N)+1;
@@ -66,7 +69,7 @@ int process_bingo(int bingo[N][N], int num){
 	int x,y;
 	
 	for(y=0; y<N; y++){
-		for(x=0;x<N;x++){
+		for(x=0; x<N; x++){
 			if(bingo[N][N]==num){
 				bingo[y][x]=0;
 			}
@@ -79,19 +82,19 @@ int print_bingo(int bingo[N][N]){
 	for (y=0; y<N; y++){
 		for(x=0; x<N; x++){
 		
-		printf("%4d", bingo[N][N]);
-			}
+		printf("%4d\n", bingo[N][N]);
 		}
-		printf("\n\n");
+	}
+	printf("\n\n");
 }
 
 
-int count_bingo(int bingo[5][5]){
+int count_bingo(int bingo[N][N]){
 	int i,j,sum,count;
 	
-	for(j=0;j<N;j++){
-		sum =0;
-		for(i=0;i<N;i++){
+	for(j=0; j<N; j++){
+		sum=0;
+		for(i=0; i<N; i++){
 			sum+=bingo[j][i];
 		}
 		if(sum==0){
@@ -124,4 +127,26 @@ int count_bingo(int bingo[5][5]){
 	return count;
 }
 
-
+int get_number_byMe(){
+	int num;
+	int error;
+	
+	do{
+		error=0;
+		printf("지울 숫자를 입력하세요: ");
+		scanf("%d", &num);
+		if(num<1||num>N*N){
+			printf("다시 입력하세요.");
+			error=1; 
+		} 
+	}while (error==1);
+}
+int get_number_byCom(){
+	int num;
+	int error;
+	
+	do{
+		error=0;
+		num = rand()%(N*N)+1;
+	}while (error==1);
+}
